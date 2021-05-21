ee190bf57a0b5bd61eeaa7a645ba1d6ee0fb8efc
Cemre Aktemur <aktemur.cemre@gmail.com>
2021-05-21 20:59:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const io = require("socket.io")((process.env.PORT || 3000), { cors: true });
 
 io.on("connection", socket => {
+	
+	  for (var i in line_history) {
+      socket.emit('drawing', { line: line_history[i] } );
+   }
+	
+	
 	socket.on("join-room", (room) => {
 		socket.join(room);
 		socket.roomId = room;
 		socket.emit("joined");
 	});
+	
 	socket.on("drawing", (data) => {
 		socket.to(socket.roomId).broadcast.emit("drawing", data);
 	});
+	
+	
 });
